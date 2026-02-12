5b1a2f495be0098677acc181eece4586c6ff2b9b
rzasnjaya <sanjayareza77@gmail.com>
2026-02-12 16:07:32 +0700

    MainMenu deepdive

--- Assets/Deep Dive/Scripts/GameManager.cs
+++ Assets/Deep Dive/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -46,6 +47,11 @@ public class GameManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
 
