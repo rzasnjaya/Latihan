35814d5a2925c74dcf27e4e273c429db0b22b153
rzasnjaya <sanjayareza77@gmail.com>
2026-02-18 17:14:47 +0700

    Player Animations

--- Assets/Fearless Feathers/Scripts/PlayerController2.cs
+++ Assets/Fearless Feathers/Scripts/PlayerController2.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class PlayerController2 : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+
     [SerializeField] private InputAction playerControls;
     [SerializeField] private Vector2 moveDirection;
     [SerializeField] private float moveSpeed;
@@ -13,6 +15,16 @@ public class PlayerController2 : MonoBehaviour
     void Update()
     {
         moveDirection = playerControls.ReadValue<Vector2>();
+        if (moveDirection != Vector2.zero)
+        {
+        animator.SetBool("moving", true);
+        animator.SetFloat("moveX", moveDirection.x);
+        animator.SetFloat("moveY", moveDirection.y);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
     }
 
     void FixedUpdate()
