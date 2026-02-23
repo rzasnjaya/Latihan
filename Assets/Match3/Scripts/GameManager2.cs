77348dff103b9d4a7de245878d1590aff58c2e9c
rzasnjaya <sanjayareza77@gmail.com>
2026-02-23 09:41:16 +0700

    smooth moves

--- Assets/Match3/Scripts/GameManager2.cs
+++ Assets/Match3/Scripts/GameManager2.cs
@@ -4,45 +4,30 @@ using UnityEngine;
 
 public class GameManager2 : Singleton<GameManager2>
 {
-    private ProjectilePool pool;
+    private MatchablePool pool;
 
     private void Start()
     {
-        pool = (ProjectilePool) ProjectilePool.Instance;
+        pool = (MatchablePool) MatchablePool.Instance;
 
-        pool.PoolObjects(5);
+        pool.PoolObjects(10);
 
         StartCoroutine(Demo());
     }
 
     private IEnumerator Demo()
     {
-        List<Projectile> projectileList = new List<Projectile>();
-        Projectile projectile;
+        Matchable m = pool.GetPooledObject();
+
+        m.gameObject.SetActive(true);
+
+        Vector3 randomPosition;
 
         for (int i = 0; i != 7; ++i)
         {
-            projectile = pool.GetPooledObject();
-            projectileList.Add(projectile);
-            projectile.Randomize();
-            projectile.gameObject.SetActive(true);
+            randomPosition = new Vector3(Random.Range(-6f, 6f), Random.Range(-4f, 4f), 0);
 
-            yield return new WaitForSeconds(0.5f);
-        }
-        for (int i = 0; i != 4; ++i)
-        {
-            pool.ReturnObjectToPool(projectileList[i]);
-
-            yield return new WaitForSeconds(0.5f);
-        }
-        for (int i = 0; i != 7; ++i)
-        {
-            projectile = pool.GetPooledObject();
-            projectileList.Add(projectile);
-            projectile.Randomize();
-            projectile.gameObject.SetActive(true);
-
-            yield return new WaitForSeconds(0.5f);
+            yield return StartCoroutine(m.MoveToPosition(randomPosition));
         }
     }
 }
---
+++ Assets/Match3/Scripts/Movable.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movable : MonoBehaviour
+{
+    private Vector3 from,
+                    to;
+    private float howfar;
+    private bool idle = true;
+
+    public bool Idle                    
+    {                                   
+        get
+        {
+            return idle;
+        }
+    }
+
+    [SerializeField]
+    private float speed = 1;
+
+    public IEnumerator MoveToPosition(Vector3 targetPosition)
+    {
+        if (speed < 0)
+        {
+            Debug.LogWarning("Speed must be a positive number");
+        }                              
+
+        from = transform.position;
+        to = targetPosition;
+        howfar = 0;
+        idle = false;
+
+        do
+        {
+            howfar += speed * Time.deltaTime;
+            if (howfar > 1)
+                howfar = 1;
+            transform.position = Vector3.LerpUnclamped(from, to, Easing(howfar));
+            yield return null;
+        }
+        while (howfar != 1);
+
+        idle = true;
+    }
+
+    private float Easing(float t)
+    {
+        float c1 = 1.70158f,
+              c2 = c1 * 1.525f;
+
+        return t < 0.5f
+            ? (Mathf.Pow(t * 2, 2) * ((c2 + 1) * 2 * t - c2)) / 2
+            : (Mathf.Pow(t * 2 - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2;
+    }
+}                                       
