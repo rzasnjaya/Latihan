e7539dda6b9c935955dc2d35e6b67a4f0f388bb3
rzasnjaya <sanjayareza77@gmail.com>
2026-03-10 15:38:20 +0700

    Level Manager

---
+++ Assets/Point&Click/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set; }
+
+    [SerializeField] GameObject panel;
+    [SerializeField] RectTransform loadBar;
+
+    private Vector3 barScale = Vector3.one;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        HidePanel();
+    }
+
+    public void SceneLoad(string sceneName)
+    {
+        StartCoroutine(AsyncLoading(sceneName));
+    }
+
+    IEnumerator AsyncLoading(string sceneName)
+    {
+        ShowPanel();
+
+        yield return new WaitForEndOfFrame();
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+            UpdateBar(progress);
+
+            yield return null;
+        }
+
+        HidePanel();
+    }
+
+    void UpdateBar(float value)
+    {
+        barScale.x = value;
+
+        loadBar.localScale = barScale;
+    }
+
+    void ShowPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    void HidePanel()
+    {
+        panel.SetActive(false);
+    }
+}
