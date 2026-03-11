9140052dfa955dd246b9ccd9517da867c0ca5b14
rzasnjaya <sanjayareza77@gmail.com>
2026-03-11 11:04:39 +0700

    Player Spawn Postition

---
+++ Assets/Point&Click/Scripts/Manager/PlayerSpawnPosition.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PlayerSpawnPosition : MonoBehaviour
+{
+    [SerializeField] List<Spawner> spawnEntries = new List<Spawner>();
+
+    private Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        Reposition();
+    }
+    
+    void Reposition()
+    {
+        for (int i = 0; i < spawnEntries.Count; i++)
+        {
+            if (DataManager.instance.PrevSceneName == spawnEntries[i].PrevSceneName)
+            {
+                player.position = spawnEntries[i].SpawnPos;
+                player.rotation = Quaternion.LookRotation(spawnEntries[i].SpawnDir);
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class Spawner
+{
+    [SerializeField] string prevSceneName;
+    [SerializeField] Vector3 spawnPos;
+    [SerializeField] Vector3 spawnDir;
+
+    public string PrevSceneName { get { return prevSceneName; } }
+    public Vector3 SpawnPos { get { return spawnPos; } }
+    public Vector3 SpawnDir { get { return spawnDir; } }
+}
